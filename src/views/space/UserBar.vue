6cc60be39ef386a884aea521381816fcf31809cd
menglingbujie <menglingbujie@gmail.com>
2019-09-19 16:40:07 +0800

    组件改为ts写法

--- src/views/space/UserBar.vue
+++ src/views/space/UserBar.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="panel userbar">
-    <div class="uname">XXXX</div>
+    <div class="uname">{{uname}}</div>
     <img src="">
   </div>
 </template>
-<script>
-export default {
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
 
+@Component
+export default class userbar extends Vue{
+  private uname:string = "Marylin"
+  mounted(){
+    console.log("===usern mount-=")
+  }
+  created():void {
+    console.log("===helloo user bar==")
+  }
 }
 </script>
 <style lang="less" scoped>
